e1cc824d5ac6781cd8f9387373392c02b71d719b
Sarat Chandra <yvssaraschandra@gmail.com>
2020-05-24 21:55:32 +0530

    Updates

---
+++ precache-manifest.b53fffe5c843e14599f2f7002c441459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac978fd9c3ae93fb29e702770a8dd005",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "3882e152c965131bd6f5",
+    "url": "/shopping-cart/static/css/main.abbf12d5.chunk.css"
+  },
+  {
+    "revision": "87370d4b975526e2912f",
+    "url": "/shopping-cart/static/js/2.efb51fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.efb51fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3882e152c965131bd6f5",
+    "url": "/shopping-cart/static/js/main.79227907.chunk.js"
+  },
+  {
+    "revision": "531342bfcad1931b43d0",
+    "url": "/shopping-cart/static/js/runtime-main.d30a4a8b.js"
+  }
+]);
